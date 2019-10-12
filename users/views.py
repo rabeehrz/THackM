78510ca7b5cf637f617f5c967367504144eafa09
divyab275 <divyab275@gmail.com>
2019-10-13 00:58:14 +0530

    ui changs

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import User
+from reports.models import Report
+from report_code.models import ReportCode
 
 # Create your views here.
 
@@ -8,8 +10,24 @@ def index(request):
 
 def login(request):
     name = request.POST.get('name', "Default")
-    person = User.objects.all().filter(name=name)
+    person = User.objects.filter(name=name)
+    reports = Report.objects.filter(lawyer = person[0].id, status=1)
+    ipc = {}
+    for report in reports:
+        codes = ReportCode.objects.filter(report=report)
+        for code in codes:
+            if report.id in ipc:
+                ipc[report.id].append(code.ipc_code)
+            else:
+                ipc[report.id] = [code.ipc_code,]
+        
+    for codes in ipc:
+        ipc[codes] = ", ".join(map(str, ipc[codes]))
+    print(ipc)    
     context = {
-        "name" : name
+        "name" : name,
+        "reports": reports,
+        "ipc": ipc
     }
     return render(request,'users/home.html',context)
+
