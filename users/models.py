8ee5ba76f50d9f64b4631eacb90538efe13517b4
divyab275 <divyab275@gmail.com>
2019-10-12 18:16:38 +0530

    templates

--- users/models.py
+++ users/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
-class User(models.Model):
-    name    = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    phone = models.CharField
+
 
