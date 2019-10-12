6ced81ba9f16f3793c368017ced1f0d1efb7612c
Rabeeh <mohammadrabeeh@gmail.com>
2019-10-12 17:54:58 +0530

    Complete: User

--- users/models.py
+++ users/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class User(models.Model):
     name    = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
-    phone = models.CharField
+    phone = models.IntegerField(null = True)
+    user_type = models.IntegerField(default=0) # 0: User #1: Lawyer
 
